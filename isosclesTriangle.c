3d70817d1644193ad3206f4e1a5e87b1837ff730
marknikky <emirhan.tasdeviren@outlook.com>
2019-12-05 17:29:38 +0300

    Added new pattern

---
+++ isosclesTriangle.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+#define N 21
+
+int evenOdd(int x) {
+    if(x % 2 == 0) {
+        return -1;
+    }
+
+    else {
+        return 0;
+    }
+}
+
+void fillTriangle(int coord[N][N], int h) {
+    int i, j, w = 0;
+    bool print;
+
+    int center  = (N-1) / 2;
+    int top     = center + (h/2);
+    int bottom  = center - (h/2) + evenOdd(h);
+
+    for(j = 0; j <= N-1; j++) {
+        for(i = 0; i <= N-1; i++) {
+            if((j <= top && j >= bottom) && (i <= (center + w) && i >= (center - w))) {
+                coord[j][i] = 1;
+                print = true;
+            }
+        }
+        if(print == true) {
+            w++;
+            print = false;
+        }
+    }
+}
+
+void drawTriangle(int coord[N][N]) {
+    int i, j;
+
+    for(j = 0; j <= N-1; j++) {
+        for(i = 0; i <= N-1; i++) {
+            if(coord[j][i] == 1) {
+                printf("*");
+            }
+            else {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main(void) {
+    int h;
+    int coord[N][N] = {0};
+
+    printf("Please enter height of your triangle:");
+    scanf("%i", &h);
+
+    fillTriangle(coord, h);
+
+    drawTriangle(coord);
+
+    return 0;
+}
